079d7d925c69696e618efbf70e057cfb7482399f
AnnZh <ann.zhurok@gmail.com>
2019-02-17 23:59:09 +0300

    Done

--- NumberFinder.cs
+++ NumberFinder.cs
@@ -6,8 +6,24 @@ namespace PadawansTask6
     {
         public static int? NextBiggerThan(int number)
         {
-            // put your code here
-            throw new NotImplementedException();
+            string s = number.ToString();
+            char[] arr = new char[s.Length];
+            arr = s.ToCharArray();
+            
+            for (int i = 0; i < arr.Length - 1; i++)
+            {              
+                if (arr[arr.Length - 1] > arr[arr.Length - i - 2]) {
+                    char tmp = arr[arr.Length - 1];
+                    arr[arr.Length - 1] = arr[arr.Length - i - 2];
+                    arr[arr.Length - i - 2] = tmp;
+                    
+                    string b = new string(arr);
+                    int a = Convert.ToInt32(b);
+                    return a;
+                }         
+            }
+
+            return 0;
         }
     }
 }
